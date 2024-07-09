6b64bb31232c40e66b00c51d658962c05b49940d
phamminhtrang11 <phamminhtrang11c@gmail.com>
2024-07-09 21:43:43 +0700

    use object mapper

--- src/main/java/com/example/logindemospringboot/Controller/LoginController.java
+++ src/main/java/com/example/logindemospringboot/Controller/LoginController.java
@@ -1,14 +1,20 @@
 package com.example.logindemospringboot.Controller;
 
 import com.example.logindemospringboot.ILogin.imp.LoginImp;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class LoginController {
 
     private final LoginImp loginImp;
+    ObjectMapper mapper = new ObjectMapper();
 
     @Autowired
     public LoginController(LoginImp loginImp) {
@@ -17,15 +23,15 @@ public class LoginController {
 
     @PostMapping("/api/login")
     @ResponseBody
-    public String checkUser(@RequestParam("username") String username, @RequestParam("password") String password) {
-        boolean loginFlag = loginImp.validUser(username, password);
-        JSONObject jsonResponse = new JSONObject();
-        if (loginFlag) {
-            jsonResponse.put("success", true);
-            jsonResponse.put("redirectURL", "/success");
-        } else {
-            jsonResponse.put("success", false);
-        }
-        return jsonResponse.toString();
+    public String checkUser(@RequestBody LoginReq req) throws JsonProcessingException {
+        // Validate
+        // dung object mapper
+        boolean loginFlag = loginImp.validUser(req.getUsername(), req.getPassword());
+        Map<String, Object> rs = new HashMap<>();
+        rs.put("success", loginFlag);
+        rs.put("redirectURL", "/success");
+        String jsonResponse = mapper.writeValueAsString(rs);
+
+        return jsonResponse;
     }
 }
